916465c91a24e8e8ecc4985d8afde757431e4fc7
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2024-03-29 18:27:39 -0300

    Feat: Compo Home

---
+++ src/Components/CompoHome/index.jsx
@@ -0,0 +1,14 @@
+import Title from "../UIComponents/Title/Title";
+
+export default function CompoHome () {
+
+ 
+
+  return(
+    <div className="bg-white p-8 w-full">
+      <Title title={'Olá, José Erisvaldo !!'} />
+    </div>
+
+
+  )
+}

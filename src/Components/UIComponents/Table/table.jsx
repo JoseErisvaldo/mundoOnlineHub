bc495be31dee208fbed1eed512b07dd86ff52734
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2024-03-29 18:27:18 -0300

    Fix: styles

--- src/Components/UIComponents/Table/table.jsx
+++ src/Components/UIComponents/Table/table.jsx
@@ -5,7 +5,7 @@ function Table({ data }) {
   return (
     <table className='w-full '>
       <thead>
-        <tr>
+        <tr className='border-b-2'>
           {/* Cria os cabeçalhos da tabela baseados nas chaves do primeiro objeto */}
           {Object.keys(data[0]).map(key => (
             <th key={key}>{key}</th>
@@ -15,10 +15,10 @@ function Table({ data }) {
       <tbody>
         {/* Mapeia os dados para criar as linhas da tabela */}
         {data.map((row, index) => (
-          <tr key={index}>
+          <tr key={index} className=''>
             {/* Mapeia cada valor do objeto para criar as células */}
             {Object.values(row).map((value, index) => (
-              <td key={index} className='text-center cursor-pointer'>{value}</td>
+              <td key={index} className='text-center cursor-pointer p-3'>{value}</td>
             ))}
           </tr>
         ))}

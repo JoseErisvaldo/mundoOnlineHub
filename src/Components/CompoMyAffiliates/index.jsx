d9db5de7253b329e13b94c2c173ff8e194c1e7d6
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2024-03-29 18:22:28 -0300

    Feat: CompoMyAffiliates

--- src/Components/CompoMyAffiliates/index.jsx
+++ src/Components/CompoMyAffiliates/index.jsx
@@ -1,22 +1,52 @@
 
-import { FaChevronLeft } from "react-icons/fa";
-import { FaChevronRight } from "react-icons/fa";
-import { FaRegCircle } from "react-icons/fa";
-import { IoIosSearch } from "react-icons/io";
+import NavBar from "../Navigation/NavBar";
 import GenericForm from "../UIComponents/Form/GenericForm";
 import Table from "../UIComponents/Table/table";
 import Title from "../UIComponents/Title/Title";
-
+import { IoSearchOutline } from "react-icons/io5";
 
 
 export default function CompoMyAffiliates () {
 
+  let search = [
+    {
+      type: 'text',
+      placeholder: 'Buscar...'
+    }
+  ] 
+
+  let dados = [
+    {"DATA": "01/03/2024", "NOME": "João", "PRODUTO": "REACT + TAILWIND CSS", "COMISSÃO": 5, "STATUS": "Ativo"},
+    {"DATA": "05/03/2024", "NOME": "Maria", "PRODUTO": "REACT + TAILWIND CSS", "COMISSÃO": 7, "STATUS": "Ativo"},
+    {"DATA": "10/03/2024", "NOME": "Pedro", "PRODUTO": "REACT + TAILWIND CSS", "COMISSÃO": 6, "STATUS": "Inativo"},
+    {"DATA": "15/03/2024", "NOME": "Ana", "PRODUTO": "REACT + TAILWIND CSS", "COMISSÃO": 8, "STATUS": "Ativo"},
+    {"DATA": "20/03/2024", "NOME": "Carlos", "PRODUTO": "REACT + TAILWIND CSS", "COMISSÃO": 5, "STATUS": "Inativo"}
+]
+
+
 
   return(
     <div className="bg-white p-8 w-full">
-     CompoMyAffiliates
-    </div>
+     <Title title={'Meus Afiliado'}/>
 
+      <div className="flex gap-3 mb-3">
+         <NavBar titleNavBar={'Ativos'} />
+         <NavBar titleNavBar={'Solicitações pendentes'} />
+         <NavBar titleNavBar={'Recusados, bloqueados ou cancelados '} />
+      </div>
+      <div className="mb-5">
+        <div className="flex items-center justify-between w-60 border p-2">
+          <GenericForm fields={search}/>
+          <div className="text-1xl cursor-pointer">
+          <IoSearchOutline/>
+          </div>
+        </div>
+      </div>
+      <div className=" overflow-x-scroll ">
+        <Table data={dados} />
+      </div>
+    </div>
+    
 
   )
 }

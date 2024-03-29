208106d524747be03f88cb8aa1919728f1fbdad4
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2024-03-29 18:26:12 -0300

    Fix: style/ templet

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,20 +1,19 @@
 
 import { IoIosNotificationsOutline } from "react-icons/io";
-import { TbProgress } from "react-icons/tb";
+
 
 
 export default function Header () {
   return(
-    <div className=" bg-blue-500 flex items-center justify-between p-4 text-white w-full">
+    <div className=" bg-blue-500 flex items-center justify-between p-4 text-white w-full ">
       <div className="flex items-center space-x-4">
-        
-        <span>joseerivaldogp@...</span>
+        <span className="hidden md:block ml-10">joseerisvaldo@...</span>
       </div>
       <div className="flex items-center space-x-4">
         
         <div className="">
           <div className="flex items-center justify-between w-full px-2 gap-5">
-            <span>R$ 5k  /</span>
+            <span>R$ 1k  /</span>
             <span>R$ 10k</span>
           </div>
         </div>

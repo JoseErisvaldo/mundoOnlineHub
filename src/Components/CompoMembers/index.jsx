2ac7b4c3449e0fb5184ecb5687f2d19f9724b226
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2024-03-29 18:22:12 -0300

    fix: style

--- src/Components/CompoMembers/index.jsx
+++ src/Components/CompoMembers/index.jsx
@@ -1,11 +1,10 @@
 
-import { FaChevronLeft } from "react-icons/fa";
-import { FaChevronRight } from "react-icons/fa";
-import { FaRegCircle } from "react-icons/fa";
 import { IoIosSearch } from "react-icons/io";
 import GenericForm from "../UIComponents/Form/GenericForm";
 import Table from "../UIComponents/Table/table";
 import Title from "../UIComponents/Title/Title";
+import FotterTable from "../UIComponents/FotterTable";
+import Guide from "../UIComponents/Guide";
 
 
 
@@ -37,24 +36,9 @@ export default function CompoMembers () {
 
       <div className="mt-6">
         <Table data={data} /> 
-        <div className="flex justify-between items-center mt-4">
-          <span className="text-sm">Exibindo 0 de 0 páginas</span>
-          <div className="flex space-x-2">
-            <button className="px-4" variant="outline">
-              <FaChevronLeft className="w-4 h-4" />
-            </button>
-            <button className="px-4" variant="outline">
-              <FaChevronRight className="w-4 h-4" />
-            </button>
-          </div>
-        </div>
+        <FotterTable/>
       </div>
-      <div className="mt-6 flex items-center">
-        <FaRegCircle className="w-5 h-5 text-blue-500 mr-2" />
-        <a className="text-blue-500 underline" href="#">
-          Aprenda mais sobre a área de membros
-        </a>
-      </div>
+      <Guide topic={'a area de membro'}/>
     </div>
 
 

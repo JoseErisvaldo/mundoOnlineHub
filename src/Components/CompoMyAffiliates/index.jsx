262cdab19fab1220be43292712b913055616c00c
joseerisvaldo <100242848+JoseErisvaldo@users.noreply.github.com>
2024-04-04 16:16:48 +0000

    Feat: request supabase

--- src/Components/CompoMyAffiliates/index.jsx
+++ src/Components/CompoMyAffiliates/index.jsx
@@ -1,52 +1,96 @@
-
+import { useEffect, useState } from "react";
+import supabase from "../../SupabaseClient";
 import NavBar from "../Navigation/NavBar";
 import GenericForm from "../UIComponents/Form/GenericForm";
 import Table from "../UIComponents/Table/table";
 import Title from "../UIComponents/Title/Title";
 import { IoSearchOutline } from "react-icons/io5";
 
-
-export default function CompoMyAffiliates () {
+export default function CompoMyAffiliates() {
+  const [affiliates, setAffiliates] = useState([]);
 
   let search = [
     {
-      type: 'text',
-      placeholder: 'Buscar...'
-    }
-  ] 
+      type: "text",
+      placeholder: "Buscar...",
+    },
+  ];
 
-  let dados = [
-    {"DATA": "01/03/2024", "NOME": "João", "PRODUTO": "REACT + TAILWIND CSS", "COMISSÃO": 5, "STATUS": "Ativo"},
-    {"DATA": "05/03/2024", "NOME": "Maria", "PRODUTO": "REACT + TAILWIND CSS", "COMISSÃO": 7, "STATUS": "Ativo"},
-    {"DATA": "10/03/2024", "NOME": "Pedro", "PRODUTO": "REACT + TAILWIND CSS", "COMISSÃO": 6, "STATUS": "Inativo"},
-    {"DATA": "15/03/2024", "NOME": "Ana", "PRODUTO": "REACT + TAILWIND CSS", "COMISSÃO": 8, "STATUS": "Ativo"},
-    {"DATA": "20/03/2024", "NOME": "Carlos", "PRODUTO": "REACT + TAILWIND CSS", "COMISSÃO": 5, "STATUS": "Inativo"}
-]
+  const fecthAffiliates = () => {
+    return new Promise((resolve, reject) => {
+      supabase
+        .from("affiliate")
+        .select("*")
+        .then((data, error) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(data.data);
+          }
+        });
+    });
+  };
 
+  const fecthUsers = () => {
+    return new Promise((resolve, reject) => {
+      supabase
+        .from("users")
+        .select("*")
+        .then((data, error) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(data.data);
+          }
+        });
+    });
+  };
 
+  const fecthProducts = () => {
+    return new Promise((resolve, reject) => {
+      supabase
+        .from("products")
+        .select("*")
+        .then((data, error) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(data.data);
+          }
+        });
+    });
+  };
 
-  return(
+  useEffect(() => {
+    Promise.all([fecthAffiliates(), fecthUsers(), fecthProducts()]).then(
+      ([affiliatesData, usersData, productsData]) => {
+        setAffiliates(affiliatesData);
+        console.log(usersData);
+        console.log(productsData);
+      },
+    );
+  }, []);
+
+  return (
     <div className="bg-white p-8 w-full">
-     <Title title={'Meus Afiliado'}/>
+      <Title title={"Meus Afiliado"} />
 
       <div className="flex gap-3 mb-3">
-         <NavBar titleNavBar={'Ativos'} />
-         <NavBar titleNavBar={'Solicitações pendentes'} />
-         <NavBar titleNavBar={'Recusados, bloqueados ou cancelados '} />
+        <NavBar titleNavBar={"Ativos"} />
+        <NavBar titleNavBar={"Solicitações pendentes"} />
+        <NavBar titleNavBar={"Recusados, bloqueados ou cancelados "} />
       </div>
       <div className="mb-5">
         <div className="flex items-center justify-between w-60 border p-2">
-          <GenericForm fields={search}/>
+          <GenericForm fields={search} />
           <div className="text-1xl cursor-pointer">
-          <IoSearchOutline/>
+            <IoSearchOutline />
           </div>
         </div>
       </div>
       <div className=" overflow-x-scroll ">
-        <Table data={dados} />
+        <Table data={affiliates} />
       </div>
     </div>
-    
-
-  )
+  );
 }

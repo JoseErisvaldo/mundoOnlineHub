d3b090a4261028db02b67cf2e31b30a6a01e9a0a
joseerisvaldo <100242848+JoseErisvaldo@users.noreply.github.com>
2024-04-05 23:08:21 +0000

    Feat: new pages

--- src/RoutesApp.jsx
+++ src/RoutesApp.jsx
@@ -17,51 +17,58 @@ import Report from "./Pages/Report";
 import Collaborators from "./Pages/Collaborators";
 import Dashboard from "./Pages/Dashboard";
 import Login from "./Pages/Login";
+import AffiliatesActive from "./Components/CompoMyAffiliates/AffiliatesActive";
+import AffiliatesPending from "./Components/CompoMyAffiliates/AffiliatesPending";
 
+export default function RoutesApp() {
+  const AdminPrivate = ({ children }) => {
+    const { autenticado } = useContext(AuthContext);
 
-
-export default function RoutesApp () {
-
-  const AdminPrivate = ({children}) => {
-    
-    const {autenticado} = useContext(AuthContext)
-
-    
-    if(!autenticado) {
-      return <Navigate to="/login"  />
+    if (!autenticado) {
+      return <Navigate to="/login" />;
     }
-    return children
-  }
+    return children;
+  };
 
-  return(
+  return (
     <BrowserRouter>
-    <AuthProvider>
+      <AuthProvider>
         <AdminPrivate>
-          <Header/>
+          <Header />
         </AdminPrivate>
-      
+
         <div className="flex">
           <AdminPrivate>
-          <SideBar/>
+            <SideBar />
           </AdminPrivate>
           <Routes>
-
-            <Route path="/login" element={  <Login/>} />
-            <Route path="/" element={ <AdminPrivate> <Home/> </AdminPrivate>} />
-            <Route path="/produtos" element={<Products/>} />
-            <Route path="/membros" element={<Members/>} />
-            <Route path="/marketplace" element={<Marketplace/>} />
-            <Route path="/meusafiliados" element={<MyAffiliates/>} />
-            <Route path="/vendas" element={<Orders/>} />
-            <Route path="/assinaturas" element={<Subscriptions/>} />
-            <Route path="/financeiro" element={<Financial/>} />
-            <Route path="/relatorios" element={<Report/>} />
-            <Route path="/colaboradores" element={<Collaborators/>} />
+            <Route path="/login" element={<Login />} />
+            <Route
+              path="/"
+              element={
+                <AdminPrivate>
+                  {" "}
+                  <Home />{" "}
+                </AdminPrivate>
+              }
+            />
+            <Route path="/produtos" element={<Products />} />
+            <Route path="/membros" element={<Members />} />
+            <Route path="/marketplace" element={<Marketplace />} />
+            <Route
+              path="/solicitacoespendentes"
+              element={<AffiliatesPending />}
+            />
+            <Route path="/afiliadosativos" element={<AffiliatesActive />} />
+            <Route path="/vendas" element={<Orders />} />
+            <Route path="/assinaturas" element={<Subscriptions />} />
+            <Route path="/financeiro" element={<Financial />} />
+            <Route path="/relatorios" element={<Report />} />
+            <Route path="/colaboradores" element={<Collaborators />} />
             <Route path="/dashboard" element={<Dashboard />} />
           </Routes>
         </div>
       </AuthProvider>
-
     </BrowserRouter>
-  )
+  );
 }

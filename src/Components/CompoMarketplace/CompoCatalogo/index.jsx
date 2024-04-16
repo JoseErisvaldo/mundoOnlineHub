df962429cc11b3cc9aea56a37160474c373d8a3a
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2024-04-16 20:28:00 -0300

    git merge

--- src/Components/CompoMarketplace/CompoCatalogo/index.jsx
+++ src/Components/CompoMarketplace/CompoCatalogo/index.jsx
@@ -44,6 +44,7 @@ export default function CompoCatalogo() {
           return { ...products, user };
         });
         setCatalog(joinProducts);
+        console.log(joinProducts)
       },
     );
   }, []);
